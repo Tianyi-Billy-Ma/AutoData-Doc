a7507cc9fe2e43e1a611146f0dca245f06904979
Tianyi-Billy-Ma <159465837+HypergraphResearcher@users.noreply.github.com>
2025-11-30 04:56:53 -0500

    INIT

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+from datetime import date
+
+# -- Project information -----------------------------------------------------
+
+project = 'AutoData'
+copyright = f'{date.today().year}, AutoData Team'
+author = 'AutoData Team'
+release = '0.2.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'myst_parser',
+    'sphinx_copybutton',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for MyST Parser -------------------------------------------------
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
